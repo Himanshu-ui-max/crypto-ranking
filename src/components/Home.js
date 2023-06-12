0cec24bbdf6033886b9c7b3adc47574b87c72ecd
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-06-12 17:31:57 +0530

    api improved

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ import Marketcap from './Marketcap';
 function Home() {
   const [parseData, setparseData] = useState([]);
   const cryptodata=async ()=>{
-    let url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=8&page=1&sparkline=false&price_change_percentage=1h&locale=en&precision=full'
+    let url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=8&page=1&sparkline=false&price_change_percentage=1h&locale=en&precision=2'
    let data=await fetch(url);
    let parsedata=await data.json();
    setparseData(parsedata);
