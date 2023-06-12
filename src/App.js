f95152cbbed7e52d15f2b95848a42eef560649f9
aryan-narang <aryannarang014@gmail.com>
2023-06-12 15:18:52 +0530

    home comp added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Homepage/>
-      <Market/>
+      <Home/>
 
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Homepage from './Homepage'
+
+function Home() {
+  return (
+    <div>
+      <Homepage/>
+      <Market/>
+    </div>
+  )
+}
+
+export default Home
