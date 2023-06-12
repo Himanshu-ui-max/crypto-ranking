5b97dfbae7cfdb83aac919449585cbb53f14735c
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-06-12 14:56:25 +0530

    table added

---
+++ src/components/Market.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function Market() {
+  return (
+    <div>
+      <table className="table">
+  <thead className="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+      <th className="col">growth</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>1.2%</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>2.4%</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td>3.4%</td>
+    </tr>
+  </tbody>
+</table>
+
+
+    </div>
+  )
+}
