d732347b6db34036c0dc6cf9af980aae19e4634e
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-06-12 16:21:30 +0530

    row elemenated from table

--- src/components/Market.js
+++ src/components/Market.js
@@ -6,7 +6,6 @@ export default function Market() {
       <table className="table">
   <thead className="thead-dark">
     <tr>
-      <th scope="col">#</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
       <th scope="col">Handle</th>
@@ -15,21 +14,21 @@ export default function Market() {
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
+      
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
       <td>1.2%</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
+      
       <td>Jacob</td>
       <td>Thornton</td>
       <td>@fat</td>
       <td>2.4%</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
+      
       <td>Larry</td>
       <td>the Bird</td>
       <td>@twitter</td>
