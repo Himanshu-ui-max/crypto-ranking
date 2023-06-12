01ff3dee67cdef23180176a8f98713e7ef853f2b
aryan-narang <aryannarang014@gmail.com>
2023-06-12 15:10:59 +0530

    homepage added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Market from './components/Market';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Homepage/>
       <Market/>
+
     </div>
   );
 }
 
 export default App;
-//hey
