1a9083335174726c84b70d7cf0508c14c91bf0b9
aryan-narang <aryannarang014@gmail.com>
2023-06-12 15:00:34 +0530

    homepage added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Market from './components/Market';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
+
       <Navbar/>
+      <Homepage/>
       <Market/>
     </div>
   );
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Homepage() {
   return (
     <div>
-      
+      <div className="container">
+        <h1>Welcome to Crypto-Ranking !</h1>
+      </div>
     </div>
   )
 }
