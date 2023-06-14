7f3439a85949dd2b26da9c73d90d12f6bcaba406
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-06-14 16:38:50 +0530

    table improved

--- src/components/Tabledata.js
+++ src/components/Tabledata.js
@@ -6,10 +6,10 @@ export default function Tabledata(props) {
       <table className="table table-borderless">
   <tbody>
     <tr className='d-flex justify-content-evenly' style={{fontSize:'1.6rem'}}>
-      <th scope="row" style={{width : '300px'}}>{props.name}</th>
-      <td style={{width : '300px'}}>{props.price}</td>
-      <td style={{width : '300px'}}>{props.change}</td>
-      <td style={{width : '300px'}}>{props.marketcap}</td>
+      <th className='tabledata' scope="row" style={{width : '300px'}}>{props.name}</th>
+      <td className='tabledata' style={{width : '300px'}}>{props.price}</td>
+      <td className='tabledata' style={{width : '300px'}}>{props.change}</td>
+      <td className='tabledata' style={{width : '300px'}}>{props.marketcap}</td>
     </tr>
     <hr />
   </tbody>
