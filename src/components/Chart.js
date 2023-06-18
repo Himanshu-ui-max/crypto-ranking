9c4262c23a5b93d10e0f45ce78b06b3037cd3a20
shubhzieee <130304179+shubhzieee@users.noreply.github.com>
2023-06-18 12:10:46 +0530

    timeline buttons added

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
+import moment from 'moment';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -41,28 +42,40 @@ ChartJS.register(
           }, []);
 
         const coinChartData = chart.prices?.map(value => ({ x: value[0],
-          y: value[1].toFixed(2) }));
+          y: value[1].toFixed(2)}));
           console.log(coinChartData);
            const options = {
             responsive: true
             }
             const data = {
-            labels: ['1', '20','2'],
+              labels: coinChartData?.map(value => moment(value.x).format('MMM DD')),
             datasets: [
             {
                 fill:true,
                 label: coinId,
-                data: ['10', '2', '90'],
+                data: coinChartData?.map(val=>val.y),
                 borderColor: 'rgb(53, 162, 235)',
                 backgroundColor: 'rgba(53, 162, 235, 0.5)',
             }
         ]
             }
   return (
+    <>
+         <div className="buttons">
+          <input type="checkbox" className="btn-check mx-2" id="btn-check-outlined" autocomplete="off"/>
+          <label className="btn btn-outline-primary" for="btn-check-outlined">1 D</label>
+          <input type="checkbox" className="btn-check mx-2" id="btn-check-2-outlined" checked autocomplete="off"/>
+          <label className="btn btn-outline-secondary" for="btn-check-2-outlined">1 W</label>
+          <input type="radio" className="btn-check mx-2" name="options-outlined" id="success-outlined" autocomplete="off" checked/>
+          <label className="btn btn-outline-success" for="success-outlined">1 M</label>
+
+          <input type="radio" className="btn-check mx-2" name="options-outlined" id="danger-outlined" autocomplete="off"/>
+          <label className="btn btn-outline-danger" for="danger-outlined">1 Y</label>
+          </div>
     <div>
       <Line options={options} data={data} />
     </div>
-   
+    </>
   )
 }
 
