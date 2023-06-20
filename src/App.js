86286b0bec8758fa27d6dd886bed6aff0e30201a
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-06-20 17:37:32 +0530

    link improved

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import CrypDetail from './components/CrypDetail';
+import { useState, useEffect } from 'react';
 
 
 import {
@@ -14,14 +15,28 @@ import {
 
 
 function App() {
+  const [parseData, setparseData] = useState();
+  const fetchData= async ()=>{
+    let url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=32&page=1&sparkline=false&price_change_percentage=1h&locale=en&precision=2'
+   let data=await fetch(url);
+   let parsedata=await data.json();
+   setparseData(parsedata);
+   
+   
+  }
+  useEffect(() => {
+    fetchData();
+   // eslint-disable-next-line
+    
+   }, []);
   return (
     <div className="App">
       <Router>
       <Navbar/>
       
       <Routes>
-        <Route exact path='/' element={<Home/>} ></Route>
-        <Route exact path='/coin/:coinId' element={<CrypDetail key="cryp" />}></Route>
+        <Route exact path='/' element={<Home/>} ></Route>" 
+        <Route exact path='/coin/:coinId' element={<CrypDetail key={parseData?.name}  />}></Route>
         {/* <Route exact path='/coin/:coinId' element={<Homepage key="cry" />}></Route> */}
       </Routes>
       </Router>
