d10bea79a48db0004ae994c4d1ade0b406116288
aryan-narang <aryannarang014@gmail.com>
2023-07-04 00:12:07 +0530

    .

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,8 +3,17 @@ import loading from './Gear.gif'
 
 export default function Spinner() {
   return (
+    <div>
     <div style={{margin : ' 100px auto'}}>
-      <img src={loading} alt='loading' style={{width : '200px', height : '200px'}} />
+      <div class="spinner-box">
+        <div class="leo-border-1">
+          <div class="leo-core-1"></div>
+        </div> 
+        <div class="leo-border-2">
+          <div class="leo-core-2"></div>
+        </div> 
+      </div>
+    </div>
     </div>
   )
 }
