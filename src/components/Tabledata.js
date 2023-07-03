47e4892e8857dbbd7f9e62a94c1bf9dc5b6d651b
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-07-03 23:11:34 +0530

    added toFixed

--- src/components/Tabledata.js
+++ src/components/Tabledata.js
@@ -19,9 +19,9 @@ export default function Tabledata(props) {
     {props.parseData.map((item)=>{
      return <Link to={`/coin/${item.id}`}  style={{textDecoration:"none", color:"black"}}>
     <tr id="tabledata"className='d-flex justify-content-evenly' style={{fontSize:'1.6rem',color:'white',padding:'20px 0px 20px 0px'}}>
-      <th className='TabD' scope="row" style={{width : '300px', textAlign : 'left'}}><img src={item.image} alt="" style={{height : '40px', width : '40px'}} /> {item.name}</th>
+      <th className='TabD' scope="row" style={{width : '300px', textAlign : 'left'}}><img src={item.image} alt="" style={{height : '40px', width : '40px'}} />{item.name}</th>
       <td className='TabD' style={{width : '300px',textAlign :'right' }}>{"$"+numberWithCommas(item.current_price.toFixed(2))}</td>
-      <td className='TabD' style={{width : '300px', textAlign :'right',color:item.price_change_percentage_24h.toFixed(2)>0?'#0ecb81':'#FF0000'}}>{item.price_change_percentage_24h.toFixed(2)>0?'+'+item.price_change_percentage_24h:item.price_change_percentage_24h}%</td>
+      <td className='TabD' style={{width : '300px', textAlign :'right',color:item.price_change_percentage_24h.toFixed(2)>0?'#0ecb81':'#FF0000'}}>{item.price_change_percentage_24h.toFixed(2)>0?'+'+item.price_change_percentage_24h.toFixed(2):item.price_change_percentage_24h.toFixed(2)}%</td>
       <td className='TabD' style={{width : '300px',textAlign :'right'}}>{"$"+numberWithCommas(item.market_cap)}</td>
     </tr>
     </Link>
