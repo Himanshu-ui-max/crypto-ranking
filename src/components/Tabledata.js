289a8b8873b7894219011149a53b8c2e847850fc
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-07-05 23:47:13 +0530

    erros due to table removed

--- src/components/Tabledata.js
+++ src/components/Tabledata.js
@@ -14,12 +14,12 @@ export default function Tabledata(props) {
     <>
     
     <div id='tablediv'>
-      <table  className="table table-borderless" style={{width: '76vw',margin: 'auto'}}>
-  <tbody >
     {props.parseData.map((item)=>{
       return (
-      <>
+        <>
       <Link to={`/coin/${item.id}`}  style={{textDecoration:"none", color:"black"}}>
+        <table  className="table table-borderless" style={{width: '76vw',margin: 'auto'}}>
+        <tbody >
        <tr id="tabledata"className='d-flex justify-content-evenly' style={{fontSize:'1.6rem',color:'white',padding:'20px 0px 20px 0px'}}>
       <th className='TabD' scope="row" style={{width : '300px', textAlign : 'left'}}><div className='d-flex'><div><img src={item.image} alt="" style={{height : '40px', width : '40px',marginLeft:'10px',marginRight:'10px'}} /></div><div>{item.name}</div></div></th>
       <td className='TabD' style={{width : '300px',textAlign :'right' }}>{"$"+numberWithCommas(item.current_price.toFixed(2))}</td>
@@ -27,13 +27,13 @@ export default function Tabledata(props) {
       <td className='TabD' style={{width : '300px',textAlign :'right'}}>{"$"+numberWithCommas(item.market_cap)}</td>
   
     </tr>
-<hr style={{padding:'0px', backgroundColor:'white',borderTop:'3px solid white' }} />
+  </tbody>
+</table>
+<hr style={{width : '76vw',padding:'0px', backgroundColor:'white',borderTop:'3px solid white' }} />
     </Link>
   </>)
   })}
   
-  </tbody>
-</table>
     </div>
     
      
