1cba57e2d66f4f6b6467f565754a602cc5a3afd8
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-07-05 11:38:35 +0530

    some errors removed

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,9 +4,9 @@ export default function Spinner() {
   return (
     <div id='loader'>
     <div style={{margin : ' 100px auto'}}>
-      <div class="spinner-box">
-        <div class="leo-border-1">
-          <div class="leo-core-1"></div>
+      <div className="spinner-box">
+        <div className="leo-border-1">
+          <div className="leo-core-1"></div>
         </div> 
        
       </div>
