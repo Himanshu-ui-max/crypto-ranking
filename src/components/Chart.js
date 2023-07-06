a6f860ef434324df24a90b87140379a6ab5c3bbb
aryan-narang <aryannarang014@gmail.com>
2023-07-06 16:55:45 +0530

    spinner added in crypdetail

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import moment from 'moment';
+import Spinner from './Spinner';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -45,11 +46,14 @@ export default function HistoryChart  () {
           chartData('365');
           setFormat('MMMM YYYY')
         }
+        const [loading, setloading] = useState(false);
         const [format, setFormat] = useState('MMMM Do , h:mm:ss a');
         const chartData = async (days) => {
             let url = `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=usd&days=${days}`;
+            setloading(true);
             let data = await fetch(url);
             let parsedata = await data.json();
+            setloading(false);
             setChart(parsedata);
             console.log(parsedata);
           }
@@ -79,6 +83,7 @@ export default function HistoryChart  () {
   return (
     <>
     <div   >
+    {loading && <div id="spinner" style={{alignItems:'center',display:'flex',justifyContent:'space-around'}}><Spinner/></div>}
             <div className="btn-group  " role="group" aria-label="Default button group" style={{margin : '50px 0px'}}>
             <button type="button" className="btn btn-outline-success rounded mx-3" onClick={handle1D} >1 D</button>
             <button type="button" className="btn btn-outline-success rounded mx-3" onClick={handle1W}>1 W</button>
