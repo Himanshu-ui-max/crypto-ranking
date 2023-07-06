6a22ed97ad1d3721a9229e4f9fbb57dce5e32c89
shubhzieee <130304179+shubhzieee@users.noreply.github.com>
2023-07-07 00:52:23 +0530

    final

--- src/components/CrypDetail.js
+++ src/components/CrypDetail.js
@@ -40,7 +40,7 @@ export default function CrypDetail() {
           <div id="detailheading" style={{ display: 'flex', justifyContent: 'space-between',fontSize:'1.2rem' }}> 
             <div className="p-2" >Current Price:{' '}<span style={{color:'#0ecb81'}}>{coin.market_data ?"$" + numberWithCommas(coin.market_data.current_price.usd.toFixed(2)) : ""}</span></div>
             <div className="p-2" >Market Cap:{' '}<span style={{color:'#0ecb81'}}>{coin.market_data ?"$" + numberWithCommas(coin.market_data.market_cap.usd.toFixed(2)) : ""}</span></div>
-            <div className="p-2" >Price Change:{' '}<span style={{color:coin.market_data ? coin.market_data.price_change_percentage_24h.toFixed(2)>0?'#0ecb81':'#FF0000': ""}}>{coin.market_data ?coin.market_data.price_change_percentage_24h.toFixed(2) + "%" : ""}</span></div>
+            <div className="p-2" >Price Change(1H):{' '}<span style={{color:coin.market_data ? coin.market_data.price_change_percentage_24h.toFixed(2)>0?'#0ecb81':'#FF0000': ""}}>{coin.market_data ?coin.market_data.price_change_percentage_24h.toFixed(2) + "%" : ""}</span></div>
           </div>
           <div className="description" id="description" style={{ height: '300px', width: '800px', overflow: 'auto', textAlign: 'left', padding: '0px',fontSize:'1.2rem',marginTop:'30px' }}>
             <p style={{color:'white'}} dangerouslySetInnerHTML={{
