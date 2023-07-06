23f448b7bf62af7a33473a62b2d342edcf08174b
Himanshu-ui-max <himanshusharma234567@gmail.com>
2023-07-06 09:21:52 +0530

    console is cleared

--- src/components/Tabledata.js
+++ src/components/Tabledata.js
@@ -10,6 +10,7 @@ export default function Tabledata(props) {
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
+  console.log = console.warn = console.error = () => {};
   return (
     <>
     
